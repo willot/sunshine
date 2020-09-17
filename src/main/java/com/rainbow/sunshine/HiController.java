61177dc401c0bef2df51a4231db2fadac6961cc5
Willot <vwillot@MGC12YX4ACLVCG.fbpld77.ford.com>
2020-09-17 14:07:03 -0400

    Confused about import

--- src/main/java/com/rainbow/sunshine/HiController.java
+++ src/main/java/com/rainbow/sunshine/HiController.java
@@ -1,6 +1,5 @@
 package com.rainbow.sunshine;
 
-import netscape.javascript.JSObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
