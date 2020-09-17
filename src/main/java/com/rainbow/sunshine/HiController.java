50b12d615b7d0a94140117d2d0d12abe5ee9d45e
Willot <vwillot@MGC12YX4ACLVCG.fbpld77.ford.com>
2020-09-17 10:28:26 -0400

    first commit

---
+++ src/main/java/com/rainbow/sunshine/HiController.java
@@ -0,0 +1,15 @@
+package com.rainbow.sunshine;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HiController {
+
+
+    @PostMapping("/sunshine/hi")
+    public ResponseEntity<String> postHi() {
+            return ResponseEntity.ok("NICE !!");
+    }
+}
---
+++ src/main/java/com/rainbow/sunshine/SunshineApplication.java
@@ -0,0 +1,13 @@
+package com.rainbow.sunshine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunshineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunshineApplication.class, args);
+	}
+
+}
