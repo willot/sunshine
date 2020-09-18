4ae210d8b5bfb4ea73c6048229c93b04da35be54
Willot <vwillot@MGC12YX4ACLVCG.fbpld77.ford.com>
2020-09-18 09:33:02 -0400

    Clean imports

--- src/main/java/com/rainbow/sunshine/HiController.java
+++ src/main/java/com/rainbow/sunshine/HiController.java
@@ -2,11 +2,9 @@ package com.rainbow.sunshine;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
