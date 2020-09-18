2dea15dd49296be72b1822656c378eb324731f89
Willot <vwillot@MGC12YX4ACLVCG.fbpld77.ford.com>
2020-09-18 10:16:29 -0400

    add welcome command

--- src/main/java/com/rainbow/sunshine/HiController.java
+++ src/main/java/com/rainbow/sunshine/HiController.java
@@ -15,14 +15,8 @@ public class HiController {
     @PostMapping("/sunshine/hi")
     public ResponseEntity<SlackResponse> postHi() {
         List emojis = Emoji.getEmoji();
-        String everything="";
 
-        for (int i = 0; i < emojis.size(); i++) {
-            everything = everything + " " + emojis.get(i);
-
-        }
-
-        SlackResponse response = new SlackResponse("in_channel", ":rainbow: :rainbow: Nice to see you today!!! :sunny: :sunny: Have a great sunny day!! :sunny: :unicorn_face: :fireworks: :pikachu_dancing:");
+        SlackResponse response = new SlackResponse("in_channel", ":rainbow: :rainbow: Nice to see you today!!! :sunny: :sunny: Have a great sunny day!! :sunny: :unicorn_face: :beach_with_umbrella: :pikachu_dancing:");
         return ResponseEntity.ok(response);
     }
 
@@ -40,8 +34,20 @@ public class HiController {
 
         glitterString = glitterString + "!!!";
 
-        SlackResponse response = new SlackResponse("in_channel", ":rainbow: :sunny: " + user_name.toUpperCase() + " :rainbow: :sunny: " + "want to know how you are doing?" + glitterString );
+        SlackResponse response = new SlackResponse("in_channel", ":rainbow: :sunny: " + user_name.toUpperCase() + " :rainbow: :sunny: " + "want to know how you are doing?" + "\n" + glitterString );
         return ResponseEntity.ok(response);
     }
 
+
+    @PostMapping("/sunshine/welcome")
+    public ResponseEntity<SlackResponse> postWelcome(@RequestParam String user_name) {
+        List emojis = Emoji.getEmoji();
+        Random random = new Random();
+
+        SlackResponse response = new SlackResponse("in_channel", ":rainbow: :sunny: " +"@"+user_name.toUpperCase() +
+                " :rainbow: :sunny: \n" + emojis.get(random.nextInt(emojis.size())) +" "+ emojis.get(random.nextInt(emojis.size())) +
+                " "+emojis.get(random.nextInt(emojis.size())) +" "+ "!!!WELCOME TO THE TEAM!!!" + emojis.get(random.nextInt(emojis.size())) +
+                " "+emojis.get(random.nextInt(emojis.size())) +" "+ emojis.get(random.nextInt(emojis.size())) +" ");
+        return ResponseEntity.ok(response);
+    }
 }
